052d8da63612f834e9f90a47a40ceac7b4edfa63
elmatadrianes3000 <157919052+elmatadrianes3000@users.noreply.github.com>
2024-02-24 13:01:28 -0800

    Add files via upload

--- baseComponents.jsx
+++ baseComponents.jsx
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
 
 class Base extends Component {
     state = { 
         titulo: "Mi primera aplicacion",
-        mensaje: "Esto es un mensaje"
+        mensaje: "Esto es un mensaje",
+        cuenta: 0
      } 
+
+     cambiarCuenta = () =>{
+        this.setState({cuenta: this.state.cuenta + 1});
+    }
+
+    restarCuenta = () =>{
+        this.setState({cuenta: this.state.cuenta - 1});
+    }
+
+    resetCuenta = () =>{
+        this.setState({cuenta:0 });
+    }
+
+
     render() { 
         return (<>
 
                 <h1>{this.state.titulo}</h1>
-                <p>{this.state.mensaje}</p>
+                <p>{this.state.cuenta}</p>
+                <button onClick={this.cambiarCuenta} className="btn btn-primary">
+                Click me
+                </button>
+
+                <button onClick={this.restarCuenta} class="btn btn-info">
+                Rest Count
+                </button>
+
+                <button onClick={this.resetCuenta} class="btn btn-danger">
+                Restart Count
+                </button>
+
                 </>       
         );
     
     }
 }
- function MyComponent() {
-  return (
-    <div>
-      <button onClick={() => alert('Button clicked!')}>
-        Click me
-      </button>
-    </div>
-  );
-}
+  
 
-export default MyComponent;
 export default Base;
